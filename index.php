b10c28f7da3a0e5a6f463346822d13bbd37facb8
Naykyr Oliveira <naykyr@hotmail.com>
2024-02-03 17:17:16 -0300

    :ok_hand: feat: Estilização do CSS

--- index.php
+++ index.php
@@ -10,99 +10,99 @@
     <title>NomeSite</title>
 </head>
 <body>
-    <main class="block__main">
-        <header class="block__main__header">
-            <h1 class="block__main__header__h1">meu site</h1>
-            <nav class="block__main__nav">
+    <main class="main">
+        <header class="main__header">
+            <h1 class="main__header__h1">meu site</h1>
+            <nav class="main__nav">
                 <ul>
-                    <li class="block__main__nav__item"><a href="#">quem somos</a></li>
-                    <li class="block__main__nav__item"><a href="#">catálogo</a></li>
-                    <li class="block__main__nav__item"><a href="#">contato</a></li>
+                    <li class="main__nav__item"><a href="#">quem somos</a></li>
+                    <li class="main__nav__item"><a href="#">catálogo</a></li>
+                    <li class="main__nav__item"><a href="#">contato</a></li>
                 </ul>
             </nav>
         </header>
-        <section id="about" class="block__about">
-            <p class="block__about__p">
+        <section id="about" class="about">
+            <p class="about__p">
                 A [nome da loja] é uma loja de eletrônicos especializada na venda de produtos importados originais de alta qualidade. Oferecemos uma ampla variedade de produtos, incluindo smartphones, smartwatches, earphones e acessórios das marcas
-                <ul class="block__about__ul">
-                    <li class="block__about__ul__item">Xiaomi</li>
-                    <li class="block__about__ul__item">POCO</li>
-                    <li class="block__about__ul__item">Huawei</li>
-                    <li class="block__about__ul__item">OnePlus</li>
-                    <li class="block__about__ul__item">Realme</li>
-                    <li class="block__about__ul__item">Anker</li>
+                <ul class="about__ul">
+                    <li class="about__ul__item">Xiaomi</li>
+                    <li class="about__ul__item">POCO</li>
+                    <li class="about__ul__item">Huawei</li>
+                    <li class="about__ul__item">OnePlus</li>
+                    <li class="about__ul__item">Realme</li>
+                    <li class="about__ul__item">Anker</li>
                 </ul>
             </p>
-            <p class="block__about__p">
+            <p class="about__p">
                 Nosso objetivo é oferecer aos nossos clientes a melhor experiência de compra possível. Trabalhamos com os melhores fornecedores do mundo para garantir que nossos produtos sejam de alta qualidade e que nossos preços sejam competitivos.
             </p>
-            <p class="block__about__p">
+            <p class="about__p">
                 Acreditamos que a tecnologia deve ser acessível a todos. É por isso que trabalhamos para oferecer uma variedade de produtos para atender a todos os orçamentos.
             </p>
-            <p class="block__about__p">
+            <p class="about__p">
                 Aqui estão alguns dos nossos diferenciais:
-                <ul class="block__about__ul">
-                    <li class="block__about__ul__item">Seleção de produtos de alta qualidade de marcas renomadas</li>
-                    <li class="block__about__ul__item">Preços competitivos</li>
-                    <li class="block__about__ul__item">Atendimento ao cliente de excelência</li>
-                    <li class="block__about__ul__item">Entrega rápida e segura</li>
-                    <li class="block__about__ul__item">Garantia de originalidade</li>
+                <ul class="about__ul">
+                    <li class="about__ul__item">Seleção de produtos de alta qualidade de marcas renomadas</li>
+                    <li class="about__ul__item">Preços competitivos</li>
+                    <li class="about__ul__item">Atendimento ao cliente de excelência</li>
+                    <li class="about__ul__item">Entrega rápida e segura</li>
+                    <li class="about__ul__item">Garantia de originalidade</li>
                 </ul>
             </p>
-            <p class="block__about__p">Visite nosso site e confira nossa seleção de produtos.</p>
+            <p class="about__p">Visite nosso site e confira nossa seleção de produtos.</p>
         </section>
-        <h2 id="productCatalog" class="block__product__catalog__h2">Catálogo</h2>
-        <section id="container" class="block__container block__container__with__button">
-            <h2 class="block__product__catalog__h2">smartphones</h2>
-            <button class="block__container__button block__container__button__prev"></button>
+        <h2 id="productCatalog" class="product__catalog__h2">Catálogo</h2>
+        <section id="container" class="container container__with__button">
+            <h2 class="product__catalog__h2">smartphones</h2>
+            <button class="container__button container__button__prev"></button>
             <div></div>
             <div></div>
             <div></div>
-            <button class="block__container__button block__container__button__next"></button>
-        </section>
-        <hr>
-        <section class="block__container">
-            <h2 class="block__product__catalog__h2">smartwatches</h2>
-            <button class="block__container__button block__container__button__prev"></button>
-            <div></div>
-            <div></div>
-            <div></div>
-            <button class="block__container__button block__container__button__next"></button>
+            <button class="container__button container__button__next"></button>
         </section>
         <hr>
-        <section class="block__container">
-            <h2 class="block__product__catalog__h2">earphones</h2>
-            <button class="block__container__button block__container__button__prev"></button>
+        <section class="container">
+            <h2 class="product__catalog__h2">smartwatches</h2>
+            <button class="container__button container__button__prev"></button>
             <div></div>
             <div></div>
             <div></div>
-            <button class="block__container__button block__container__button__next"></button>
+            <button class="container__button container__button__next"></button>
         </section>
         <hr>
-        <section class="block__container">
-            <h2 class="block__product__catalog__h2">dispositivos e acessórios</h2>
-            <button class="block__container__button block__container__button__prev"></button>
+        <section class="container">
+            <h2 class="product__catalog__h2">earphones</h2>
+            <button class="container__button container__button__prev"></button>
             <div></div>
             <div></div>
             <div></div>
-            <button class="block__container__button block__container__button__next"></button>
+            <button class="container__button container__button__next"></button>
+        </section>
+        <hr>
+        <section class="container">
+            <h2 class="product__catalog__h2">dispositivos e acessórios</h2>
+            <button class="container__button container__button__prev"></button>
+            <div></div>
+            <div></div>
+            <div></div>
+            <button class="container__button container__button__next"></button>
         </section>
     </main>
-    <footer id="contact" class="block__footer">
-        <div class="block__footer__div block__footer__div__with__instagram">
-            <h2 class="block__footer__h2">contato</h2>
-            <h3 class="block__footer__h3">instagram</h3>
-            <p class="block__footer__p">Entre em contato, basta entrar nos <br>
+    <footer id="contact" class="footer">
+        <div class="footer__div footer__div__with__instagram">
+            <h2 class="footer__h2">contato</h2>
+            <h3 class="footer__h3">instagram</h3>
+            <p class="footer__p">Entre em contato, basta entrar nos <br>
                 nosso instagram ou acessar o <br>
                 whatspp.</p>
             <a href="#">@intagram</a>
             <a href="#">Link para acesso</a>
         </div>
-        <div class="block__footer__div block__footer__div__with__form">
+        <div class="footer__div footer__div__with__form">
             Ficaram  dúvidas?
-            <p class="block__footer__p">Para dúvidas  nos deixe uma mensagem.</p>
+            <p class="footer__p">Para dúvidas  nos deixe uma mensagem.</p>
         </div>
-        <p class="block__footer__p">Desenvolvido por <a href="#" target="_blank">.</a></p>
+        <p class="footer__p">Desenvolvido por <a href="#" target="_blank">.</a></p>
     </footer>
     <script src="assets/js/script.js"></script>
 </body>
